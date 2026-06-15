c2b0c4b3f04bc7b6a743d961ccaf02d9f76d23ad
Matheus Ferraz Portela Da Silva <matheussferraz567@gmail.com>
2026-06-14 21:13:23 -0300

    Add files via upload

---
+++ conversor_fatorial.py
@@ -0,0 +1,7 @@
+import math # Importação da Biblioteca Math
+
+numero = int(input("Digite um número: ")) # Solicitação de um número do tipo Inteiro como descrito
+resultado = math.factorial(numero) # Para realizar o cálculo usei uma função já pronta da biblioteca do Python
+print(f"O resultado é ", resultado) # Mostrar na tela o resultado adquirido.
+
+
